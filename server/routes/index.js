995380c05c287eda11184b6f65306c368a337acb
Sofia Montagna <montagnasofia@hotmail.com>
2021-04-07 14:10:59 +0000

    init

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'HOME', link: '/page2', array: ['elemento1', 'elemento2']});
 });
 
+router.get('/page2', function(req, res, next) {
+  res.render('page2', {array: ['elemento1', 'elemento2']});
+});
 module.exports = router;
